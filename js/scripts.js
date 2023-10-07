708df19fead5d832133abf534f3b7ee61b85a067
Lelly Oliver <lellyoliver@outlook.com>
2023-10-07 15:20:51 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,19 @@ function getStrUrl(name, phone, message) {
     window.location.href = concatURL;
 }
 
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
